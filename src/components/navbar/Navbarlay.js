995e8587bda1019f47e44a6e12dc702ef6957cc7
Abhilash Saj <abhilashsaj@gmail.com>
2019-08-11 23:31:02 +0530

    enhanced nav and landing page

--- src/components/navbar/Navbarlay.js
+++ src/components/navbar/Navbarlay.js
@@ -1,30 +1,47 @@
 import React from 'react'
 
 import {Navbar, Nav} from 'react-bootstrap'
+
+var navStyle = {
+   
+    paddingRight: "40px",
+    
+  };
+
+  var navStyle2 = {
+   
+    paddingRight: "20px",
+    
+  };
+  var navStyle3 = {
+    paddingRight: "40px",
+    paddingRight: "20px",
+    
+  };
 function Navbarlay() {
     return (
         <nav className="navbar navbar-expand-md navbar-dark bg-dark">
             <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                        <Nav.Link href ="/">Home</Nav.Link>
+                        <Nav.Link href ="/" style={navStyle3}>Home</Nav.Link>
                     </li>
                     <li className="nav-item">
-                        <Nav.Link href="/about">About</Nav.Link>
+                        <Nav.Link href="/about" style={navStyle}>About</Nav.Link>
                     </li>
                     <li className="nav-item">
-                        <Nav.Link href="/rescue">Rescue</Nav.Link>
+                        <Nav.Link href="/rescue" style={navStyle}>Rescue</Nav.Link>
                     </li>
                     <li className="nav-item">
-                        <Nav.Link href="/volunteer">Volunteer</Nav.Link>
+                        <Nav.Link href="/volunteer" style={navStyle}>Volunteer</Nav.Link>
                     </li>
                     
                 </ul>
             </div>
             <div className="mx-auto order-0">
-            {/* <Navbar.Brand link to="https://www.higgs.in" ><img src="https://www.higgs.in/images/LogoSm-min.png" alt="" style={{width:"130px", height: "50px"}}/> */}
-            <Navbar.Brand className="navbar-brand mx-auto" href="/">  Human Foundation &nbsp; </Navbar.Brand>
-            {/* </Navbar.Brand> */}
+            <Navbar.Brand link to="https://www.higgs.in" ><img src="./logowhite.png" alt="not loaded" style={{width:"190px", height: "70px"}}/>
+            {/* <Navbar.Brand className="navbar-brand mx-auto" href="/">  Human Foundation &nbsp; </Navbar.Brand> */}
+            </Navbar.Brand>
                 
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
                     <span className="navbar-toggler-icon"></span>
@@ -34,7 +51,7 @@ function Navbarlay() {
                 <ul className="navbar-nav ml-auto">
                     
                     <li className="nav-item">
-                        <Nav.Link className="nav-link" href="/signin">Sign In</Nav.Link>
+                        <Nav.Link className="nav-link" href="/signin" style={navStyle2}>Sign In</Nav.Link>
                     </li>
                 </ul>
             </div>
