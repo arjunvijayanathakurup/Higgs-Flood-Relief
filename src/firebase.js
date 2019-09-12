e25ddbd1b892ddc6dea813ef053afa7420750378
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2019-09-12 22:56:25 +0530

    removed details

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCChCTbnqe1z_SKozmj3_6BaAbZKtoOR9Q",
-    authDomain: "higgs-flood-relief.firebaseapp.com",
-    databaseURL: "https://higgs-flood-relief.firebaseio.com",
-    projectId: "higgs-flood-relief",
-    storageBucket: "higgs-flood-relief.appspot.com",
-    messagingSenderId: "999857828472",
-    appId: "1:999857828472:web:bc7bf29282561d53"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
   firebase.initializeApp(firebaseConfig);
